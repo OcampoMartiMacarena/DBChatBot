5f86736fe6372824e87c2a4943f8bfcc936b0e99
Maxwan <maxwan@Maxwans-MacBook-Pro.local>
2024-10-26 00:25:04 -0300

    corrected commit

--- HFlet/main.py
+++ HFlet/main.py
@@ -1,8 +1,20 @@
 import flet as ft
 from presentation.chat_view import ChatView
+from presentation.chat_model_presenter import ChatPresenter
 
 def main(page: ft.Page):
+    page.title = "Chat Application"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
     chat_view = ChatView(on_send=lambda _: None, expand=True)
-    page.add(chat_view)
+    chat_presenter = ChatPresenter(view=chat_view, page=page)
+
+    chat_presenter.set_on_send_handler(chat_view)
+
+    end_button = ft.ElevatedButton("End Conversation", on_click=chat_presenter.end_conversation)
+
+    page.add(chat_view, end_button)
+    chat_presenter.start_chat()
 
 ft.app(target=main)
